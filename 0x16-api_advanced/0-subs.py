3d61039f6ad27acb50cd564c95f54ceab387d0fd
root <sominrenson@gamil.com>
2024-06-05 04:34:22 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-""" Reddit api."""
+"""
+Return the total number of subscribers on a given subreddit.
+If an invalid subreddit is given, return 0.
+"""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers."""
+    """
+    Return the total number of subscribers on a given subreddit.
+    If an invalid subreddit is given, return 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
