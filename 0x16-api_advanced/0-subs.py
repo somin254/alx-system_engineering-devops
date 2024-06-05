5538955491306b7056441edb28dfb994b2c16a45
root <sominrenson@gamil.com>
2024-06-05 04:16:44 -0700

    apiu

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -13,4 +12,6 @@ def number_of_subscribers(subreddit):
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
-    return results.get("subscribers")
+    if results is None:
+        return 0
+    return results.get("subscribers", 0)
